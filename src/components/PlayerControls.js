c6a3cd96d68bed012846c6faac094cb74e9816cb
memoray <102960499+memoray@users.noreply.github.com>
2022-06-14 14:41:09 +0200

    first working draft

---
+++ src/components/PlayerControls.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { FaPlay, FaBackward, FaForward, FaPause } from "react-icons/fa";
+
+const PlayerControls = (props) => {
+  return (
+    <div className="c-player--controls">
+      <button className="skip-btn" onClick={() => props.SkipSong(false)}>
+        <FaBackward />
+      </button>
+      <button
+        className="play-btn"
+        onClick={() => props.setIsPlaying(!props.isPlaying)}
+      >
+        {props.isPlaying ? <FaPause /> : <FaPlay />}
+      </button>
+
+      <button className="skip-btn" onClick={() => props.SkipSong()}>
+        <FaForward />
+      </button>
+    </div>
+  );
+};
+
+export default PlayerControls;
---
+++ src/songsData.js
@@ -0,0 +1,45 @@
+const songsData = [
+  {
+    title: "Let This Go",
+    artist: "112",
+    img_src: "./img/112.jpg",
+    src: "./music/112-Let_This_Go.mp3",
+  },
+  {
+    title: "Fill Me In",
+    artist: "Craig David",
+    img_src: "./img/cd.jpg",
+    src: "./music/CD-Fill_Me_In.mp3",
+  },
+  {
+    title: "Salio El Sol",
+    artist: "Don Omar",
+    img_src: "./img/don_2.jpg",
+    src: "./music/Don_Omar-Salio_El_Sol.mp3",
+  },
+  {
+    title: "Empire State Of Mind",
+    artist: "Jay-Z",
+    img_src: "./img/Blueprint_3.jpg",
+    src: "./music/jay-z-empire_state_of_mind.mp3",
+  },
+  {
+    title: "Spaceship",
+    artist: "Kanye West",
+    img_src: "./img/college_dropout.jpg",
+    src: "./music/Kan-Spaceship.mp3",
+  },
+  {
+    title: "Day 'N' Nite",
+    artist: "Kid Cudi",
+    img_src: "./img/cudi.jpg",
+    src: "./music/kid_cudi-day_n_night.mp3",
+  },
+  {
+    title: "We Are One",
+    artist: "Pitbull",
+    img_src: "./img/pitbull.png",
+    src: "./music/Pitbull-We_Are_One_(Ole_Ola).mp3",
+  },
+];
+export default songsData;

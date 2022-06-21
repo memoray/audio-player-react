5838dbaa5da13c99622c3ab25a31b8cd80c2efbb
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-06-21 17:14:02 +0200

    add api request

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import Player from "./components/Player";
-import songsData from "./songsData";
+//import songsData from "./songsData";
+import axios from "axios";
 
 function App() {
-  const [songs] = useState(songsData);
+	const apiURL = 'http://localhost:8000/api/songs',
+		[songs, setSongs] = useState([]),
+		[currentSongIndex, setCurrentSongIndex] = useState(0),
+		[nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+	useEffect(() => {
+		axios.get(apiURL)
+			.then(resp => setSongs(resp.data))
+			.catch(err => console.error(err));
 
-  useEffect(() => {
-    setNextSongIndex(() => {
-      if (currentSongIndex + 1 > songs.length - 1) {
-        return 0;
-      } else {
-        return currentSongIndex + 1;
-      }
-    });
-  }, [currentSongIndex]);
+		setNextSongIndex(() => {
+			if (currentSongIndex + 1 > songs.length - 1) {
+				return 0;
+			} else {
+				return currentSongIndex + 1;
+			}
+		});
+	}, [songs, currentSongIndex]);
 
-  return (
-    <div className="App">
-      <Player
-        currentSongIndex={currentSongIndex}
-        setCurrentSongIndex={setCurrentSongIndex}
-        nextSongIndex={nextSongIndex}
-        songs={songs}
-      />
-    </div>
-  );
+	if(songs && songs.length > 0) {
+		return (
+			<div className="App">
+				<Player
+					currentSongIndex={currentSongIndex}
+					setCurrentSongIndex={setCurrentSongIndex}
+					nextSongIndex={nextSongIndex}
+					songs={songs}
+				/>
+			</div>
+		);
+	}
+	return (<div><h3>No Songs available</h3></div>);
 }
 
 export default App;
--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import { FaPlay, FaBackward, FaForward, FaPause } from "react-icons/fa";
+import React from "react";
+import {FaPlay, FaBackward, FaForward, FaPause} from "react-icons/fa";
 
 const PlayerControls = (props) => {
-  return (
-    <div className="c-player--controls">
-      <button className="skip-btn" onClick={() => props.SkipSong(false)}>
-        <FaBackward />
-      </button>
-      <button
-        className="play-btn"
-        onClick={() => props.setIsPlaying(!props.isPlaying)}
-      >
-        {props.isPlaying ? <FaPause /> : <FaPlay />}
-      </button>
+	return (
+		<div className="c-player--controls">
+			<button className="skip-btn" onClick={() => props.SkipSong(false)}>
+				<FaBackward/>
+			</button>
+			<button
+				className="play-btn"
+				onClick={() => props.setIsPlaying(!props.isPlaying)}
+			>
+				{props.isPlaying ? <FaPause/> : <FaPlay/>}
+			</button>
 
-      <button className="skip-btn" onClick={() => props.SkipSong()}>
-        <FaForward />
-      </button>
-    </div>
-  );
+			<button className="skip-btn" onClick={() => props.SkipSong()}>
+				<FaForward/>
+			</button>
+		</div>
+	);
 };
 
 export default PlayerControls;

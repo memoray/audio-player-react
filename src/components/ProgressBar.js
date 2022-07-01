e13f2b9ac50f32866238951e8a39d73c61020e25
memoray <102960499+memoray@users.noreply.github.com>
2022-07-01 13:48:46 +0200

    add functionality to progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,42 @@
 import React from "react";
+import { useEffect } from "react";
 
 const ProgressBar = (props) => {
+  const audio = props.audio;
+
+  const timeFormat = (time) => {
+    let min = Math.floor(time / 60)
+      .toString()
+      .padStart(2, "0");
+
+    let sec = Math.floor(time % 60)
+      .toString()
+      .padStart(2, "0");
+
+    return `${min}:${sec}`;
+  };
+
+  useEffect(() => {
+    audio.current.addEventListener("timeupdate", () => {
+      document.getElementById("current-time").innerHTML = timeFormat(
+        audio.current.currentTime
+      );
+
+      document.getElementById("bar").value =
+        (audio.current.currentTime / audio.current.duration) * 100;
+    });
+  });
+
+  useEffect(() => {
+    audio.current.addEventListener("loadeddata", function () {
+      if (audio.current.readyState >= 1) {
+        document.getElementById("duration").innerHTML = timeFormat(
+          audio.current.duration
+        );
+      }
+    });
+  });
+
   return (
     <div className="player-progress">
       <div className="prg-bar">
